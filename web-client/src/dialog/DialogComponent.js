feacaf61eebea65b85d78f00853dee758216f156
Andrei Verdes <verdes.andreivlad@gmail.com>
2018-05-12 12:27:20 +0300

    adds stimuli select and style

--- web-client/src/dialog/DialogComponent.js
+++ web-client/src/dialog/DialogComponent.js
@@ -1,21 +1,66 @@
 import React from 'react';
 import './dialog.css';
 import Dialog from 'react-dialog'
+import imageUploadIco from "../assets/image-upload-logo.ico";
+import drawIco from "../assets/draw-logo.svg";
+import youtubeIco from "../assets/youtube-logo.svg";
+
+const PickDialog = (props) => {
+    const { events } = props;
+    return <div className="stimuli-container">
+        <div className="stimuli-option" onClick={events.imageClick}>
+            <img alt='logo' src={String(imageUploadIco)} />
+        </div>
+        <div className="stimuli-option">
+            <img alt='logo' src={String(drawIco)} />
+        </div>
+        <div className="stimuli-option">
+            <img alt='logo' src={String(youtubeIco)} />
+        </div>
+    </div>
+}
+
+const ImageDialog = () => <div>ImageDialog</div>
 
 export default class DialogComponent extends React.Component {
+    constructor() {
+        super();
 
-    render() {
-        const { isDialogOpen, handleClose } = this.props;
-        const dialog = isDialogOpen ? <Dialog
-            title="Titlu"
+        this.state = { type: 0 };
+    }
+
+    getDialogView = (isDialogOpen, handleClose) => {
+        if (!isDialogOpen) return null;
+
+        const events = {
+            imageClick: this.imageClick
+        }
+
+        let currentDialogView;
+
+        switch (this.state.type) {
+            case 0: currentDialogView = <PickDialog events={events} />; break;
+            case 1: currentDialogView = <ImageDialog />; break;
+            default: currentDialogView = <PickDialog events={events} />; break;
+        }
+
+        return <Dialog
+            title="Ce postezi pe wall?"
             modal={true}
             onClose={handleClose}
         >
-            <h1>THis will be the content</h1>
-            <p>yeeeep</p>
-        </Dialog> : null;
+            {currentDialogView}
+        </Dialog>
+    }
 
-        return dialog;
+    imageClick = () => {
+        this.setState({ type: 1 });
+    }
+
+    render() {
+        const { isDialogOpen, handleClose } = this.props;
+
+        return this.getDialogView(isDialogOpen, handleClose);
 
     }
 
