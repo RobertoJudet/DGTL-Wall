ba604c5d173ec339ee000b3378933e500f5b06e8
Dejan Nichsici <nichsici.dejan@gmail.com>
2018-05-12 04:21:00 +0300

    adds fake content, hides scroll

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import DialogComponent from './dialog/DialogComponent'
 
@@ -14,14 +13,33 @@ class App extends React.Component {
   openDialog = () => this.setState({ isDialogOpen: true })
   handleClose = () => this.setState({ isDialogOpen: false })
 
+  fillCanvasWithJunk = () => {
+    const settings = this.props.settings;
+    let junkArray = [];
+    for(let i = 0; i < settings.canvasWidth; i+=100){
+      for(let j = 0; j < settings.canvasHeight; j+=100){
+        junkArray.push(<div className="junk" key={`${i}${j}`}></div>);
+      }
+        
+    }
+    return junkArray; 
+}
+
   render() {
+    const { data, settings } = this.props;
+
+    const canvasStyle = {
+      width: settings.canvasWidth,
+      height: settings.canvasHeight
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <button type="button" onClick={this.openDialog}>test dialog</button>
+      <div className="canvas" style={canvasStyle}>
+        {this.fillCanvasWithJunk()}
+
+        {/* Remove button when click event on body is in place */}
+        <button style={{position: "absolute", top:0, left:0}}type="button" onClick={this.openDialog}>test dialog</button>
+        
         <DialogComponent
           isDialogOpen={this.state.isDialogOpen}
           handleClose={this.handleClose}
